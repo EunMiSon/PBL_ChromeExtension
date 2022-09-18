f434c1fb5926b575cb221e0255dc3869130148b5
ywww22 <dleognl@swu.ac.kr>
2022-09-18 22:56:48 +0900

    필터링한 단어의 개수 출력

--- script.js
+++ script.js
@@ -13,13 +13,20 @@
 	  //bodyText의 모든 단어를 추출하고, 그 단어의 숫자를 센다. 그 결과를 bodyNum이라는 변수에 담는다. 
 	  var bodyNum = bodyText.split(' ').length;
 
+	  // 각 단계별 키워드 개수를 담을 변수 초기화
+	  var myNum1 = 0;
+	  var myNum2 = 0;
+	  var myNum3 = 0;
+	  var myNum4 = 0;
+
 	  //bodyText에서 키워드가 몇번 등장하는지를 알아본다. 그 결과를 myNum이라는 변수에 담는다.
 	  var myNum = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|안내 권유 전화|텔레마케팅|정기결제|정기 결제|자동납부|이전|개인정보 국외 이전|이관|빈도|접속 빈도 파악|통계|3자|제3자 제공|고유식별|고유식별정보|민감정보/g).length;
-	  //var myNum1 = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g).length;
-	  //var myNum2 = bodyText.match(/정기결제|자동납부/g).length;
-	  //var myNum3 = bodyText.match(/이전|이관|빈도|통계|3자/g).length;
-	  //var myNum4 = bodyText.match(/고유식별|고유식별정보|민감정보/g).length;
-
+	 
+	  // 각 단계별 키워드가 있는지 검사하고 있으면 그 값으로 설정함
+	  if(bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g) != null)  myNum1 = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g).length;
+	  if(bodyText.match(/정기결제|자동납부/g) != null)	  myNum2 = bodyText.match(/정기결제|자동납부/g).length;
+	  if(bodyText.match(/이전|이관|빈도|통계|3자/g) != null)  myNum3 = bodyText.match(/이전|이관|빈도|통계|3자/g).length;
+	  if(bodyText.match(/고유식별|고유식별정보|민감정보/g) != null)  myNum4 = bodyText.match(/고유식별|고유식별정보|민감정보/g).length;
 
 
 	  // id값이 result인 태그에 결과를 추가한다. 
