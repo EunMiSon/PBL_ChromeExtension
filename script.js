ab19c9620455024794f2df3c641a4bf8c2678445
ywww22 <dleognl@swu.ac.kr>
2022-10-06 20:26:52 +0900

    필터링 단어 들어간 문장 보여주기 기능

--- script.js
+++ script.js
@@ -4,37 +4,63 @@
 
 	//컨텐츠 페이지를 대상으로 코드를 실행해주세요. 
 	chrome.tabs.executeScript({
-	  code: 'document.querySelector("body").innerText'
-	}, function (result) {
-	  // 위의 코드가 실행된 후에 이 함수를 호출해주세요. 그 때 result에 담아주세요. 
-   
-	  //이 문서에서 body  태그 아래에 있는 모든 텍스를 가져온다. 그 결과를 bodyText라는 변수에 담는다.
-	  var bodyText = result[0];
-	  //bodyText의 모든 단어를 추출하고, 그 단어의 숫자를 센다. 그 결과를 bodyNum이라는 변수에 담는다. 
-	  var bodyNum = bodyText.split(' ').length;
-
-	  // 각 단계별 키워드 개수를 담을 변수 초기화
-	  var myNum1 = 0;
-	  var myNum2 = 0;
-	  var myNum3 = 0;
-	  var myNum4 = 0;
-
-	  //bodyText에서 키워드가 몇번 등장하는지를 알아본다. 그 결과를 myNum이라는 변수에 담는다.
-	  var myNum = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|안내 권유 전화|텔레마케팅|정기결제|정기 결제|자동납부|이전|개인정보 국외 이전|이관|빈도|접속 빈도 파악|통계|3자|제3자 제공|고유식별|고유식별정보|민감정보/g).length;
+		code: 'document.querySelector("body").innerText'
+	  }, function (result) {
+		// 위의 코드가 실행된 후에 이 함수를 호출해주세요. 그 때 result에 담아주세요. 
 	 
-	  // 각 단계별 키워드가 있는지 검사하고 있으면 그 값으로 설정함
-	  if(bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g) != null)  myNum1 = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g).length;
-	  if(bodyText.match(/정기결제|자동납부/g) != null)	  myNum2 = bodyText.match(/정기결제|자동납부/g).length;
-	  if(bodyText.match(/이전|이관|빈도|통계|3자/g) != null)  myNum3 = bodyText.match(/이전|이관|빈도|통계|3자/g).length;
-	  if(bodyText.match(/고유식별|고유식별정보|민감정보/g) != null)  myNum4 = bodyText.match(/고유식별|고유식별정보|민감정보/g).length;
-
-	  // 위험도 점수 계산
-	  var score = ((myNum1*10 + myNum2*20 + myNum3*30 + myNum4*40)/bodyNum) * 100;
-	  score = Math.round((score + Number.EPSILON) * 100) / 100;
-
-	  // id값이 result인 태그에 결과를 추가한다. 
-	  document.querySelector('#result').innerText = bodyText;
-	  document.querySelector('#test').innerText = "위험도 점수 : " + score;
-   
-   
-	});
+		//이 문서에서 body  태그 아래에 있는 모든 텍스트를 가져온다. 그 결과를 bodyText라는 변수에 담는다.
+		var bodyText = result[0];
+		//bodyText의 모든 단어를 추출하고, 그 단어의 숫자를 센다. 그 결과를 bodyNum이라는 변수에 담는다. 
+		var bodyNum = bodyText.split(' ').length;
+		//var text = bodyText.split(' ');
+  
+		var words = ['이벤트', '광고', '마케팅', '홍보', '푸쉬', '푸시', '권유', '텔레마케팅', '정기결제', '자동납부', '이전', '이관', '빈도', '통계', '3자', '고유식별', '고유식별정보', '민감정보'];  // 필터링할 단어 배열
+		var idx = "";  // 필터링할 단어의 시작 위치
+		var start = 0;  // 단어가 들어있는 문장의 시작 위치 (그 문장의 앞 문장의 끝을 .으로 찾음)
+		var end = 0;    // 단어가 들어있는 문장의 끝 위치 (역시 .으로 끝을 찾음)
+		var text = "";  // 찾아낸 문장들
+		var num = 1;
+  
+		for(var i = 0;i<words.length;i++){
+		  var indices = [];
+		  text = text + "<<< " + words[i] + " >>>" + "\n";
+		  idx = bodyText.indexOf(words[i]);
+		  while ( idx != -1 ){
+			  indices.push(idx);
+			  idx = bodyText.indexOf(words[i], idx+1);
+		  }
+			  
+		  for(var j = 0; j < indices.length; j++){
+			  start = bodyText.lastIndexOf(".", indices[j]);
+			  end = bodyText.indexOf(".", start+1);
+				  
+			  text = text + num + " ========" + "\n" + bodyText.substring(start+1, end+1) + "\n\n";
+			  num = num + 1;
+		  }
+		}
+		  
+  
+		var myNum1 = 0;
+		var myNum2 = 0;
+		var myNum3 = 0;
+		var myNum4 = 0;
+  
+		//bodyText에서 키워드가 몇번 등장하는지를 알아본다. 그 결과를 myNum이라는 변수에 담는다.
+		var myNum = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|안내 권유 전화|텔레마케팅|정기결제|정기 결제|자동납부|이전|개인정보 국외 이전|이관|빈도|접속 빈도 파악|통계|3자|제3자 제공|고유식별|고유식별정보|민감정보/g).length;
+  
+		if(bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g) != null)  myNum1 = bodyText.match(/이벤트|광고|마케팅|홍보|푸쉬|푸시|권유|텔레마케팅/g).length;
+		if(bodyText.match(/정기결제|자동납부/g) != null)	  myNum2 = bodyText.match(/정기결제|자동납부/g).length;
+		if(bodyText.match(/이전|이관|빈도|통계|3자/g) != null)  myNum3 = bodyText.match(/이전|이관|빈도|통계|3자/g).length;
+		if(bodyText.match(/고유식별|고유식별정보|민감정보/g) != null)  myNum4 = bodyText.match(/고유식별|고유식별정보|민감정보/g).length;
+  
+		// 위험도 점수 계산
+		var score = ((myNum1*10 + myNum2*20 + myNum3*30 + myNum4*40)/bodyNum) * 100;
+		score = Math.round((score + Number.EPSILON) * 100) / 100;
+  
+		// id값이 result인 태그에 결과를 추가한다. 
+		document.querySelector('#con').innerText = text;
+		//document.querySelector('#test').innerText = "전체 단어 수 : " + bodyNum + ", 위험 키워드1 수 : " + myNum1 + ", 위험 키워드2 수 : " + myNum2 + ", 위험 키워드3 수 : " + myNum3 + ", 위험 키워드4 수 : " + myNum4;
+		document.querySelector('#circle').innerText = score;
+	 
+	 
+	  });
